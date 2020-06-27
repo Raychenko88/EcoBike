fe00eceb01845371ed7c84c448002f68db2677d3
Sergey <sergeyraychenko@gmail.com>
2020-06-27 11:00:53 +0300

    work with collections 2

--- src/main/java/com/ecobike/repository/CollectionBike.java
+++ src/main/java/com/ecobike/repository/CollectionBike.java
@@ -4,11 +4,12 @@ import com.ecobike.model.ElectricBike;
 import com.ecobike.model.Speedelec;
 import com.ecobike.model.FoldingBike;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class CollectionBike {
 
-    public static Set<FoldingBike> foldingBikes;
-    public static Set<ElectricBike> electricBikes;
-    public static Set<Speedelec> speedelecs;
+    public static Set<FoldingBike> foldingBikes = new HashSet<>();
+    public static Set<ElectricBike> electricBikes = new HashSet<>();
+    public static Set<Speedelec> speedelecs = new HashSet<>();
 }
--- src/main/java/com/ecobike/service/FileManagerService.java
+++ src/main/java/com/ecobike/service/FileManagerService.java
@@ -13,6 +13,7 @@ import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class FileManagerService {
 
@@ -28,15 +29,15 @@ public class FileManagerService {
         File file = new File(fileName);
         Scanner scanner = new Scanner(System.in);
         try (FileWriter writer = new FileWriter(file, true)) {
-                writer.write(String.valueOf(scanner.nextLine()));
-                scanner.close();
+            writer.write(String.valueOf(scanner.nextLine()));
+            scanner.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    public static List<Object> getDataFromFile(String fileName){
+    public static List<Object> getDataFromFile(String fileName) {
         List<Object> items = new ArrayList<>();
 
         try {
@@ -46,65 +47,95 @@ public class FileManagerService {
             FileReader fileReader = new FileReader(decode(file.getAbsolutePath()));
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String temp;
-            while ((temp = bufferedReader.readLine()) != null){
+            while ((temp = bufferedReader.readLine()) != null) {
                 items.add(temp);
             }
-            String[] subStr;
-            for (Object item : items) {
-                String str = item.toString();
-                String delimeter = ";"; // Разделитель
-                if (item.toString().startsWith("SPEEDELEC")){
-                    subStr = str.split(delimeter);
-                    Speedelec speedelec = new Speedelec();
-                            speedelec.setBrand(subStr[0]);
-                            speedelec.setMaximumSpeed(Integer.parseInt(subStr[1].trim()));
-                            speedelec.setWeight(Integer.parseInt(subStr[2].trim()));
-                            speedelec.setLightsAtFrontAndBack(Boolean.parseBoolean(subStr[3].trim()));
-                            speedelec.setBatteryCapacity(Integer.parseInt(subStr[4].trim()));
-                            speedelec.setColor(subStr[5].trim());
-                            speedelec.setPrice(Integer.parseInt(subStr[6].trim()));
-                    CollectionBike.speedelecs.add(speedelec);
-                    System.out.println("ADD wit SPEEDELEC");
-                }else if (item.toString().startsWith("E-BIKE")){
-                    subStr = str.split(delimeter);
-                    ElectricBike electricBike = new ElectricBike(
-                            subStr[0],
-                            Integer.parseInt(subStr[1].trim()),
-                            Integer.parseInt(subStr[2].trim()),
-                            Boolean.parseBoolean(subStr[3].trim()),
-                            Integer.parseInt(subStr[4].trim()),
-                            subStr[5].trim(),
-                            Integer.parseInt(subStr[6].trim()));
-                    CollectionBike.electricBikes.add(electricBike);
-                    System.out.println("ADD wit E-BIKE");
-                }else if (item.toString().startsWith("FOLDING")){
-                    subStr = str.split(delimeter);
-                    FoldingBike foldingBike = new FoldingBike(
-                            subStr[0],
-                            Integer.parseInt(subStr[1].trim()),
-                            Integer.parseInt(subStr[2].trim()),
-                            Integer.parseInt(subStr[3].trim()),
-                            Boolean.parseBoolean(subStr[4].trim()),
-                            subStr[5].trim(),
-                            Integer.parseInt(subStr[6].trim()));
-                    CollectionBike.foldingBikes.add(foldingBike);
-                    System.out.println("ADD wit FOLDING");
-                    System.out.println(item);
-                }
-            }
+            addToCollectionSpeedelec(items);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return items;
     }
 
-    private static String decode(String path){
+    private static String decode(String path) {
         try {
             return URLDecoder.decode(path, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
         return "";
+    }
+
+
+    public static Set<Speedelec> addToCollectionSpeedelec(List<Object> items) {
+        String[] subStr;
+        for (Object item : items) {
+            String str = item.toString();
+            String delimeter = ";"; // Разделитель
+            if (item.toString().startsWith("SPEEDELEC")) {
+                subStr = str.split(delimeter);
+                Speedelec speedelec = new Speedelec();
+                speedelec.setBrand(subStr[0]);
+                speedelec.setMaximumSpeed(Integer.parseInt(subStr[1].trim()));
+                speedelec.setWeight(Integer.parseInt(subStr[2].trim()));
+                speedelec.setLightsAtFrontAndBack(Boolean.parseBoolean(subStr[3].trim()));
+                speedelec.setBatteryCapacity(Integer.parseInt(subStr[4].trim()));
+                speedelec.setColor(subStr[5].trim());
+                speedelec.setPrice(Integer.parseInt(subStr[6].trim()));
+                CollectionBike.speedelecs.add(speedelec);
+                System.out.println("ADD wit SPEEDELEC");
+            }
+        }
+        return CollectionBike.speedelecs;
+    }
+
+
+    public static Set<ElectricBike> addToCollectionElectricBike(List<Object> items) {
+        String[] subStr;
+        for (Object item : items) {
+            String str = item.toString();
+            String delimeter = ";"; // Разделитель
+            if (item.toString().startsWith("E-BIKE")) {
+                subStr = str.split(delimeter);
+                ElectricBike electricBike = new ElectricBike(
+                        subStr[0],
+                        Integer.parseInt(subStr[1].trim()),
+                        Integer.parseInt(subStr[2].trim()),
+                        Boolean.parseBoolean(subStr[3].trim()),
+                        Integer.parseInt(subStr[4].trim()),
+                        subStr[5].trim(),
+                        Integer.parseInt(subStr[6].trim()));
+                CollectionBike.electricBikes.add(electricBike);
+                System.out.println("ADD wit E-BIKE");
+            }
+        }
+        return CollectionBike.electricBikes;
+    }
+
+
+    public static Set<FoldingBike> addToCollectionFoldingBike(List<Object> items) {
+        String[] subStr;
+        for (Object item : items) {
+            String str = item.toString();
+            String delimeter = ";"; // Разделитель
+            if (item.toString().startsWith("FOLDING")) {
+                subStr = str.split(delimeter);
+                FoldingBike foldingBike = new FoldingBike(
+                        subStr[0],
+                        Integer.parseInt(subStr[1].trim()),
+                        Integer.parseInt(subStr[2].trim()),
+                        Integer.parseInt(subStr[3].trim()),
+                        Boolean.parseBoolean(subStr[4].trim()),
+                        subStr[5].trim(),
+                        Integer.parseInt(subStr[6].trim()));
+                CollectionBike.foldingBikes.add(foldingBike);
+                System.out.println("ADD wit FOLDING");
+                System.out.println(item);
+            }
+        }
+        return CollectionBike.foldingBikes;
     }
 }
 
